4b6554b922ecd4cda28be4b700739a2b2f6f1c3b
Leo Albert <leo.albert.96@web.de>
2023-04-03 13:43:13 +0200

    added restart btn

--- js/script.js
+++ js/script.js
@@ -20,8 +20,6 @@ function logShape(i) {
     };
 
     fieldstatus[i] = shapestatus;
-
-    console.log(fieldstatus);
     checkForWin();
 }
 
@@ -40,31 +38,53 @@ function checkForThree(a, b, c) {
     let winline = document.getElementById(`winline${a}${b}${c}`);
 
     if (fieldstatus[a] == fieldstatus[b] && fieldstatus[b] == fieldstatus[c] && fieldstatus[a]) {
-        console.log('Winner: ' + fieldstatus[a]);
         gameover = true;
         endScreen();
         if(winline.getAttribute('style').indexOf('transform: rotate(-45deg)') != -1){
             console.log('dia-neg');
             winline.style.transform = 'rotate(-45deg) scaleX(1)';
-
         }else{
-            if(winline.getAttribute('style').indexOf('transform:') != -1){
+            if(winline.getAttribute('style').indexOf('transform: rotate(45deg)') != -1){
                 console.log('diagonal');
                 winline.style.transform = 'rotate(45deg) scaleX(1)';
-
             }else{
                 if(winline.classList.contains('make-vertical')){
                     console.log('vertical');
                     winline.style.transform = 'rotate(90deg) scaleX(1)';
-
                 }else {
                     console.log('horizontal');
-                    winline.style.transform = 'scaleX(1)';
-                    
+                    winline.style.transform = 'scaleX(1)';                    
                 }
             }
         }   
     }
+}
+
+function restart(){
+    gameover = false;
+    fieldstatus = [];
+    resetWinlines();
+
+    for(let i = 0; i < 9; i++){
+        document.getElementById('circle-' + i).classList.add('hide');
+        document.getElementById('cross-' + i).classList.add('hide');
+    };
+
+    setTimeout(function(){
+    document.getElementById('gameover').classList.add('hide');
+    document.getElementById('restartbutton').classList.add('hide');
+    }, 1000);
+}
+
+function resetWinlines(){
+    document.getElementById('winline012').style.transform = ('scaleX(0)');
+    document.getElementById('winline345').style.transform = ('scaleX(0)');
+    document.getElementById('winline678').style.transform = ('scaleX(0)');
+    document.getElementById('winline036').style.transform = ('scaleX(0)');
+    document.getElementById('winline147').style.transform = ('scaleX(0)');
+    document.getElementById('winline258').style.transform = ('scaleX(0)');
+    document.getElementById('winline048').style.transform = ('rotate(45deg) scaleX(0)');
+    document.getElementById('winline246').style.transform = ('rotate(-45deg) scaleX(0)');
 }
 
 function endScreen(){
